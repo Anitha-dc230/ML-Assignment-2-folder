7dd4c7a0b7548c033fb529f3efc0dfc935df4c95
Anitha-dc230 <2024dc04230@wilp.bits-pilani.ac.in>
2026-02-15 00:59:02 +0530

    Refactor Streamlit app for obesity level prediction

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,152 +1,105 @@
-{
-  "cells": [
-    {
-      "cell_type": "code",
-      "execution_count": null,
-      "metadata": {
-        "colab": {
-          "base_uri": "https://localhost:8080/"
-        },
-        "collapsed": true,
-        "id": "Uj8jPIPNU_JP",
-        "outputId": "1fb70136-04f8-439a-ccae-f0bb6885b8c8"
-      },
-      "outputs": [],
-      "source": [
-        "!pip install streamlit\n",
-        "import streamlit as st\n",
-        "import pandas as pd\n",
-        "import joblib"
-      ]
-    },
-    {
-      "cell_type": "code",
-      "execution_count": null,
-      "metadata": {
-        "colab": {
-          "base_uri": "https://localhost:8080/"
-        },
-        "id": "cYXDClsKVESU",
-        "outputId": "25ed4315-eae4-4a01-c04b-9e3474694e29"
-      },
-      "outputs": [],
-      "source": [
-        "from IPython.utils.sysinfo import num_cpus\n",
-        "st.title(\"Obesity Level Classification App\")\n",
-        "\n",
-        "# Load models\n",
-        "lr = joblib.load(\"model/logistic_regression_model.pkl\")\n",
-        "dt = joblib.load(\"model/decision_tree_model.pkl\")\n",
-        "knn = joblib.load(\"model/knn_model.pkl\")\n",
-        "nb = joblib.load(\"model/naive_bayes_model.pkl\")\n",
-        "rf = joblib.load(\"model/random_forest_model.pkl\")\n",
-        "xgb = joblib.load(\"model/xgboost_model.pkl\")\n",
-        "pipeline = joblib.load(\"model/preprocessing_pipeline.pkl\")\n",
-        "\n",
-        "model_option = st.selectbox(\n",
-        "    \"Select Model\",\n",
-        "    [\"Logistic Regression\", \"Decision Tree\", \"KNN\", \"Naive Bayes\", \"Random Forest\", \"XGBoost\"]\n",
-        ")\n",
-        "\n",
-        "FEATURE_COLUMNS = ['Gender', 'Age', 'Height', 'Weight', 'family_history_with_overweight',\n",
-        "       'FAVC', 'FCVC', 'NCP', 'CAEC', 'SMOKE', 'CH2O', 'SCC', 'FAF', 'TUE',\n",
-        "       'CALC', 'MTRANS']\n",
-        "\n",
-        "st.write(\n",
-        "    \"Enter your data on the left and click **Predict** to see your obesity level.\"\n",
-        ")\n",
-        "\n",
-        "st.sidebar.header(\"Your Health details\")\n",
-        "# -----------------------------\n",
-        "# Inputs\n",
-        "# -----------------------------\n",
-        "gender = st.sidebar.selectbox(\"Gender\", [\"Male\", \"Female\"])\n",
-        "gender_f = 1 if gender == \"Female\" else 0\n",
-        "age = st.sidebar.number_input(\"Age\", min_value=0, max_value=100, value=25)\n",
-        "height = st.sidebar.number_input(\"Height\", min_value=0.0, max_value=7.0, value=5.5)\n",
-        "weight = st.sidebar.number_input(\"Weight\", min_value=0.0, max_value=500.0, value=100.0)\n",
-        "family_history_with_overweight = st.sidebar.selectbox(\"Family History with Overweight\", [\"Yes\", \"No\"])\n",
-        "family_history_with_overweight_y = 1 if family_history_with_overweight == \"Yes\" else 0\n",
-        "favc = st.sidebar.selectbox(\"Frequent consumption of high-calorie food\", [\"Yes\", \"No\"])\n",
-        "favc_y = 1 if favc == \"Yes\" else 0\n",
-        "fcvc = st.sidebar.number_input(\"Frequency of consumption of vegetables\", min_value=0, max_value=3, value=1)\n",
-        "ncp = st.sidebar.number_input(\"Number of main meals\", min_value=0, max_value=5, value=2)\n",
-        "caec = st.sidebar.selectbox(\"Consumption of alcohol\", [\"Yes\", \"No\"])\n",
-        "caec_y = 1 if caec == \"Yes\" else 0\n",
-        "smoke = st.sidebar.selectbox(\"Smoking\", [\"Yes\", \"No\"])\n",
-        "smoke_y = 1 if smoke == \"Yes\" else 0\n",
-        "ch2o = st.sidebar.number_input(\"Consumption of water per day\", min_value=0.0, max_value=10.0, value=2.0)\n",
-        "scc = st.sidebar.selectbox(\"Consumption of fruit per day\", [\"Yes\", \"No\"])\n",
-        "scc_y = 1 if scc == \"Yes\" else 0\n",
-        "faf = st.sidebar.number_input(\"Frequency of consumption of fast food\", min_value=0, max_value=3, value=1)\n",
-        "tue = st.sidebar.number_input(\"Consumption of vegetables per day\", min_value=0, max_value=3, value=1)\n",
-        "calc = st.sidebar.selectbox(\"Consumption of alcohol per day\", [\"Yes\", \"No\"])\n",
-        "calc_y = 1 if calc == \"Yes\" else 0\n",
-        "mtrans = st.sidebar.selectbox(\"Transportation used\", [\"Automobile\", \"Motorbike\", \"Bike\", \"Public Transportation\", \"Walking\"])\n",
-        "mtrans_value = 0 if mtrans == \"Automobile\" else 1 if mtrans == \"Motorbike\" else 2 if mtrans == \"Bike\" else 3 if mtrans == \"Public Transportation\" else 4\n",
-        "\n",
-        "\n",
-        "data_in = {\n",
-        "    FEATURE_COLUMNS[0]: gender_f,\n",
-        "    FEATURE_COLUMNS[1]: age,\n",
-        "    FEATURE_COLUMNS[2]: height,\n",
-        "    FEATURE_COLUMNS[3]: weight,\n",
-        "    FEATURE_COLUMNS[4]: family_history_with_overweight_y,\n",
-        "    FEATURE_COLUMNS[5]: favc_y,\n",
-        "    FEATURE_COLUMNS[6]: fcvc,\n",
-        "    FEATURE_COLUMNS[7]: ncp,\n",
-        "    FEATURE_COLUMNS[8]: caec_y,\n",
-        "    FEATURE_COLUMNS[9]: smoke_y,\n",
-        "    FEATURE_COLUMNS[10]: ch2o,\n",
-        "    FEATURE_COLUMNS[11]: scc_y,\n",
-        "    FEATURE_COLUMNS[12]: faf,\n",
-        "    FEATURE_COLUMNS[13]: tue,\n",
-        "    FEATURE_COLUMNS[14]: calc_y,\n",
-        "    FEATURE_COLUMNS[15]: mtrans_value\n",
-        "}\n",
-        "\n",
-        "data = pd.DataFrame([data_in], columns=FEATURE_COLUMNS)\n",
-        "\n",
-        "#uploaded_file = st.file_uploader(\"Upload CSV file for prediction\", type=[\"csv\"])\n",
-        "\n",
-        "#if uploaded_file is not None:\n",
-        "    #data = pd.read_csv(uploaded_file)\n",
-        "data_processed = pipeline.transform(data)\n",
-        "\n",
-        "if st.button(\"Predict my obesity level\"):\n",
-        "\n",
-        "        if model_option == \"Logistic Regression\":\n",
-        "                prediction = lr.predict(data_processed)\n",
-        "        elif model_option == \"Decision Tree\":\n",
-        "                prediction = dt.predict(data_processed)\n",
-        "        elif model_option == \"KNN\":\n",
-        "                prediction = knn.predict(data_processed)\n",
-        "        elif model_option == \"Naive Bayes\":\n",
-        "                prediction = nb.predict(data_processed)\n",
-        "        elif model_option == \"Random Forest\":\n",
-        "                prediction = rf.predict(data_processed)\n",
-        "        else:\n",
-        "                prediction = xgb.predict(data_processed)\n",
-        "\n",
-        "        st.write(\"Predictions:\")\n",
-        "        st.write(prediction)\n",
-        "else:\n",
-        "    st.info(\"Fill the details on the left and click **Predict my obesity level**.\")\n"
-      ]
-    }
-  ],
-  "metadata": {
-    "colab": {
-      "provenance": []
-    },
-    "kernelspec": {
-      "display_name": "Python 3",
-      "name": "python3"
-    },
-    "language_info": {
-      "name": "python"
-    }
-  },
-  "nbformat": 4,
-  "nbformat_minor": 0
+import streamlit as st
+import pandas as pd
+import joblib
+
+from IPython.utils.sysinfo import num_cpus
+st.title("Obesity Level Classification App")
+
+# Load models
+lr = joblib.load("model/logistic_regression_model.pkl")
+dt = joblib.load("model/decision_tree_model.pkl")
+knn = joblib.load("model/knn_model.pkl")
+nb = joblib.load("model/naive_bayes_model.pkl")
+rf = joblib.load("model/random_forest_model.pkl")
+xgb = joblib.load("model/xgboost_model.pkl")
+pipeline = joblib.load("model/preprocessing_pipeline.pkl")
+
+model_option = st.selectbox(
+    "Select Model",
+    ["Logistic Regression", "Decision Tree", "KNN", "Naive Bayes", "Random Forest", "XGBoost"]
+)
+
+FEATURE_COLUMNS = ['Gender', 'Age', 'Height', 'Weight', 'family_history_with_overweight',
+       'FAVC', 'FCVC', 'NCP', 'CAEC', 'SMOKE', 'CH2O', 'SCC', 'FAF', 'TUE',
+       'CALC', 'MTRANS']
+
+st.write(
+    "Enter your data on the left and click **Predict** to see your obesity level."
+)
+
+st.sidebar.header("Your Health details")
+# -----------------------------
+# Inputs
+# -----------------------------
+gender = st.sidebar.selectbox("Gender", ["Male", "Female"])
+gender_f = 1 if gender == "Female" else 0
+age = st.sidebar.number_input("Age", min_value=0, max_value=100, value=25)
+height = st.sidebar.number_input("Height", min_value=0.0, max_value=7.0, value=5.5)
+weight = st.sidebar.number_input("Weight", min_value=0.0, max_value=500.0, value=100.0)
+family_history_with_overweight = st.sidebar.selectbox("Family History with Overweight", ["Yes", "No"])
+family_history_with_overweight_y = 1 if family_history_with_overweight == "Yes" else 0
+favc = st.sidebar.selectbox("Frequent consumption of high-calorie food", ["Yes", "No"])
+favc_y = 1 if favc == "Yes" else 0
+fcvc = st.sidebar.number_input("Frequency of consumption of vegetables", min_value=0, max_value=3, value=1)
+ncp = st.sidebar.number_input("Number of main meals", min_value=0, max_value=5, value=2)
+caec = st.sidebar.selectbox("Consumption of alcohol", ["Yes", "No"])
+caec_y = 1 if caec == "Yes" else 0
+smoke = st.sidebar.selectbox("Smoking", ["Yes", "No"])
+smoke_y = 1 if smoke == "Yes" else 0
+ch2o = st.sidebar.number_input("Consumption of water per day", min_value=0.0, max_value=10.0, value=2.0)
+scc = st.sidebar.selectbox("Consumption of fruit per day", ["Yes", "No"])
+scc_y = 1 if scc == "Yes" else 0
+faf = st.sidebar.number_input("Frequency of consumption of fast food", min_value=0, max_value=3, value=1)
+tue = st.sidebar.number_input("Consumption of vegetables per day", min_value=0, max_value=3, value=1)
+calc = st.sidebar.selectbox("Consumption of alcohol per day", ["Yes", "No"])
+calc_y = 1 if calc == "Yes" else 0
+mtrans = st.sidebar.selectbox("Transportation used", ["Automobile", "Motorbike", "Bike", "Public Transportation", "Walking"])
+mtrans_value = 0 if mtrans == "Automobile" else 1 if mtrans == "Motorbike" else 2 if mtrans == "Bike" else 3 if mtrans == "Public Transportation" else 4
+
+
+data_in = {
+    FEATURE_COLUMNS[0]: gender_f,
+    FEATURE_COLUMNS[1]: age,
+    FEATURE_COLUMNS[2]: height,
+    FEATURE_COLUMNS[3]: weight,
+    FEATURE_COLUMNS[4]: family_history_with_overweight_y,
+    FEATURE_COLUMNS[5]: favc_y,
+    FEATURE_COLUMNS[6]: fcvc,
+    FEATURE_COLUMNS[7]: ncp,
+    FEATURE_COLUMNS[8]: caec_y,
+    FEATURE_COLUMNS[9]: smoke_y,
+    FEATURE_COLUMNS[10]: ch2o,
+    FEATURE_COLUMNS[11]: scc_y,
+    FEATURE_COLUMNS[12]: faf,
+    FEATURE_COLUMNS[13]: tue,
+    FEATURE_COLUMNS[14]: calc_y,
+    FEATURE_COLUMNS[15]: mtrans_value
 }
+
+data = pd.DataFrame([data_in], columns=FEATURE_COLUMNS)
+
+#uploaded_file = st.file_uploader("Upload CSV file for prediction", type=["csv"])
+
+#if uploaded_file is not None:
+    #data = pd.read_csv(uploaded_file)
+data_processed = pipeline.transform(data)
+
+if st.button("Predict my obesity level"):
+
+        if model_option == "Logistic Regression":
+                prediction = lr.predict(data_processed)
+        elif model_option == "Decision Tree":
+                prediction = dt.predict(data_processed)
+        elif model_option == "KNN":
+                prediction = knn.predict(data_processed)
+        elif model_option == "Naive Bayes":
+                prediction = nb.predict(data_processed)
+        elif model_option == "Random Forest":
+                prediction = rf.predict(data_processed)
+        else:
+                prediction = xgb.predict(data_processed)
+
+        st.write("Predictions:")
+        st.write(prediction)
+else:
+    st.info("Fill the details on the left and click **Predict my obesity level**.")
