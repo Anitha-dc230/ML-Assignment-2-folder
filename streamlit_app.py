f266e83b6fc4e6f6a302b154e41346ea372d1531
Anitha-dc230 <2024dc04230@wilp.bits-pilani.ac.in>
2026-02-15 10:31:43 +0530

    Remove input fields and data processing for prediction
    Removed input fields and data preparation for obesity prediction.

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,61 +53,6 @@ if uploaded_file is not None:
 
     data_processed = pipeline.transform(X_test)
 
-'''
-# -----------------------------
-# Inputs
-# -----------------------------
-gender = st.sidebar.selectbox("Gender", ["Male", "Female"])
-gender_f = 1 if gender == "Female" else 0
-age = st.sidebar.number_input("Age", min_value=0, max_value=100, value=25)
-height = st.sidebar.number_input("Height", min_value=0.0, max_value=7.0, value=5.5)
-weight = st.sidebar.number_input("Weight", min_value=0.0, max_value=500.0, value=100.0)
-family_history_with_overweight = st.sidebar.selectbox("Family History with Overweight", ["Yes", "No"])
-family_history_with_overweight_y = 1 if family_history_with_overweight == "Yes" else 0
-favc = st.sidebar.selectbox("Frequent consumption of high-calorie food", ["Yes", "No"])
-favc_y = 1 if favc == "Yes" else 0
-fcvc = st.sidebar.number_input("Frequency of consumption of vegetables", min_value=0, max_value=3, value=1)
-ncp = st.sidebar.number_input("Number of main meals", min_value=0, max_value=5, value=2)
-caec = st.sidebar.selectbox("Consumption of alcohol", ["Yes", "No"])
-caec_y = 1 if caec == "Yes" else 0
-smoke = st.sidebar.selectbox("Smoking", ["Yes", "No"])
-smoke_y = 1 if smoke == "Yes" else 0
-ch2o = st.sidebar.number_input("Consumption of water per day", min_value=0.0, max_value=10.0, value=2.0)
-scc = st.sidebar.selectbox("Consumption of fruit per day", ["Yes", "No"])
-scc_y = 1 if scc == "Yes" else 0
-faf = st.sidebar.number_input("Frequency of consumption of fast food", min_value=0, max_value=3, value=1)
-tue = st.sidebar.number_input("Consumption of vegetables per day", min_value=0, max_value=3, value=1)
-calc = st.sidebar.selectbox("Consumption of alcohol per day", ["Yes", "No"])
-calc_y = 1 if calc == "Yes" else 0
-mtrans = st.sidebar.selectbox("Transportation used", ["Automobile", "Motorbike", "Bike", "Public Transportation", "Walking"])
-mtrans_value = 0 if mtrans == "Automobile" else 1 if mtrans == "Motorbike" else 2 if mtrans == "Bike" else 3 if mtrans == "Public Transportation" else 4
-
-
-data_in = {
-    FEATURE_COLUMNS[0]: gender_f,
-    FEATURE_COLUMNS[1]: age,
-    FEATURE_COLUMNS[2]: height,
-    FEATURE_COLUMNS[3]: weight,
-    FEATURE_COLUMNS[4]: family_history_with_overweight_y,
-    FEATURE_COLUMNS[5]: favc_y,
-    FEATURE_COLUMNS[6]: fcvc,
-    FEATURE_COLUMNS[7]: ncp,
-    FEATURE_COLUMNS[8]: caec_y,
-    FEATURE_COLUMNS[9]: smoke_y,
-    FEATURE_COLUMNS[10]: ch2o,
-    FEATURE_COLUMNS[11]: scc_y,
-    FEATURE_COLUMNS[12]: faf,
-    FEATURE_COLUMNS[13]: tue,
-    FEATURE_COLUMNS[14]: calc_y,
-    FEATURE_COLUMNS[15]: mtrans_value
-}
-
-data = pd.DataFrame([data_in], columns=FEATURE_COLUMNS)
-
-
-data_processed = pipeline.transform(data)
-'''
-
 if st.button("Predict my obesity level"):
 
         if model_option == "Logistic Regression":
